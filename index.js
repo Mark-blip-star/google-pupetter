ad0c5e34463c4e6c7f04dc199e7328a537b70d01
Mark <82019875+Mark-blip-star@users.noreply.github.com>
2023-07-26 17:19:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { firefox } = require("playwright");
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getEmailContent(email, password) {
+  try {
+    const browser = await firefox.launch({
+      headless: false,
+    });
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://mail.google.com");
+    await delay(1000);
+
+    await page.waitForSelector('input[type="email"]', { visible: true });
+    await delay(2000);
+    await page.type('input[type="email"]', email);
+    await page.press('input[type="email"]', "Enter");
+
+    await page.waitForSelector('input[type="password"]', { visible: true });
+    await delay(2000);
+    await page.type('input[type="password"]', password);
+    await delay(1000);
+    await page.press('input[type="password"]', "Enter");
+
+    await page.waitForLoadState("networkidle");
+    await page.waitForSelector(".bsU");
+
+    const divElement = await page.waitForSelector(".bsU");
+    const content = await divElement.textContent();
+
+    console.log("Messages count:", content);
+    await delay(1000);
+
+    await browser.close();
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
+getEmailContent("furr.development@gmail.com", "ShNA2nTqLD");
