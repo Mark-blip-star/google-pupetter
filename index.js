eeead330cfd160d194854e204a59f256cd91b4ac
Mark <82019875+Mark-blip-star@users.noreply.github.com>
2023-07-28 19:49:06 +0300

    update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function getEmailContent(email, password) {
   try {
     const browser = await puppeteer.launch({
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      headless: false,
       ignoreDefaultArgs: [
         "--enable-automation",
         "--disable-extensions",
