00011c6fcdbcc9db7171ce1cc531e708db4ae4f4
Mark <82019875+Mark-blip-star@users.noreply.github.com>
2023-07-28 15:23:42 +0300

    add docket

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ function delay(ms) {
 async function getEmailContent(email, password) {
   try {
     const browser = await puppeteer.launch({
-      headless: false,
-      userDataDir: "/opt/render/.cache/puppeteer",
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
